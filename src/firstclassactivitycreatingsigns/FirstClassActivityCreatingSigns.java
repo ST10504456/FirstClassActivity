94cc521edd6d47df6f0156a9000bf60fdfb18910
ST10504456 <ST10504456@myemeris.edu.za>
2026-02-24 12:35:04 +0200

    First Task Submission

---
+++ src/firstclassactivitycreatingsigns/FirstClassActivityCreatingSigns.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package firstclassactivitycreatingsigns;
+
+/**
+ *
+ * @author ST10504456
+ */
+public class FirstClassActivityCreatingSigns {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /**
+         * In the below codes print and println methods were used in creating
+         * two different signs along with escape sequences.
+         */
+        
+        System.out.print("$$$$$$$$$\t");
+        System.out.println("***********");
+        System.out.print("$\t$\t");
+        System.out.println("* \t  *");
+        System.out.print("$ STOP\t$\t");
+        System.out.println("*   GO\t  *");
+        System.out.print("$\t$\t");
+        System.out.println("* \t  *");
+        System.out.print("$$$$$$$$$\t");       
+        System.out.println("***********");
+        System.out.print("   $$       \t");
+        System.out.println("    **");
+        System.out.print("   $$       \t");
+        System.out.println("    **");
+        System.out.print("   $$         \t");
+        System.out.println("    **");
+        System.out.print("   $$         \t");
+        System.out.println("    **");
+        System.out.print("  $$$$         \t");
+        System.out.println("  *******");
+        System.out.print("$$$$$$$$$\t");
+        System.out.println("***********");
+
+
+        
+
+       
+        
+        
+      
+    }
+   
+}
